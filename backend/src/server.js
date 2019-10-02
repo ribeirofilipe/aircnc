138956638ee6d2525ae3e1f425f245689e66b961
Filipe de Oliveira Ribeiro <foribeiro@viceri.com.br>
2019-10-02 00:29:30 -0300

    primeira aula pratica, do OmniStack concluída

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const routes = require('./routes')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-gibik.mongodb.net/OmniStack?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+app.use(express.json())
+app.use(routes)
+
+app.listen(3030)
