3b8d121129bc781afe3fe09848966567a310ffd5
Filipe de Oliveira Ribeiro <foribeiro@viceri.com.br>
2019-10-09 20:04:17 -0300

    inserido front

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,23 +1,22 @@
-const express = require('express')
+const express = require("express");
 
-const SessionController = require('./controllers/SessionController')
-const SpotController = require('./controllers/SpotController')
-const DashboardController = require('./controllers/DashboardController')
-const BookingController = require('./controllers/BookingController')
+const SessionController = require("./controllers/SessionController");
+const SpotController = require("./controllers/SpotController");
+const DashboardController = require("./controllers/DashboardController");
+const BookingController = require("./controllers/BookingController");
 
-const multer = require('multer')
-const uploadConfig = require('./config/upload')
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
 
-const routes = express.Router()
-const upload = multer(uploadConfig)
+const routes = express.Router();
+const upload = multer(uploadConfig);
 
-routes.post('/sessions', SessionController.store)
+routes.post("/sessions", SessionController.store);
 
-routes.get('/spots', SpotController.index)
-routes.post('/spots', upload.single('thumbnail'), SpotController.store)
+routes.get("/spots", SpotController.index);
+routes.post("/spots", upload.single("thumbnail"), SpotController.store);
 
-routes.get('/dashboard', DashboardController.show)
+routes.get("/dashboard", DashboardController.show);
+routes.post("/spots/:spot_id/bookings", BookingController.store);
 
-routes.post('/spots/:spot_id/bookings', BookingController.store)
-
-module.exports = routes
+module.exports = routes;
